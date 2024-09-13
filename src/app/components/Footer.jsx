bff53b0c35a0cd0353fb1e362ba76c9fec4c1345
castillomiguel25 <Miguelangelcastillocolmenares@gmail.com>
2024-09-12 23:22:40 -0400

    'hola'

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <Footer className="bottom-0 w-full bg-black border-gray-200 py-2.5">
-      <div> Buenas noches </div>
-      
-    </Footer>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Footer
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <footer className="fixed bottom-0 w-full bg-black border-t border-green-700">
+      <div className="flex justify-around items-center p-4 text-center text-white">
+        <Link href="/" passHref>
+          <p className="flex flex-col items-center text-white hover:text-green-500 transition duration-300 cursor-pointer">
+            <span className="text-xs">INICIO</span>
+          </p>
+        </Link>
+
+        <div className="relative flex flex-col items-center">
+          <button
+            className="flex flex-col items-center text-white hover:text-green-500 transition duration-300"
+            onClick={toggleMenu}
+          >
+            <span className="text-xs">USUARIO</span>
+          </button>
+          {menuOpen && (
+            <div className="absolute bottom-12 right-0 bg-black border border-green-700 text-white mt-2 rounded-lg shadow-lg p-2 w-40">
+              <Link href="/User" passHref>
+                <p className="block px-4 py-2 text-center hover:bg-green-500 transition duration-300 cursor-pointer rounded">
+                  Configuración
+                </p>
+              </Link>
+
+              <Link href="/User" passHref>
+                <p className="block px-4 py-2 text-center hover:bg-green-500 transition duration-300 cursor-pointer rounded">
+                  hola miquea
+                </p>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
